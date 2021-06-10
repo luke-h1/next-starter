d8d6c45609cbeadc8fbc2782785535158990eb33
luke-h1 <luke.howsam@yahoo.com>
2021-06-10 08:50:44 +0100

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-cd ..
 npm run build
 npm run format
 npm run lint 
 npm run tsc
 npm run test 
-npm run cy:ci 
+npm run cypress:ci
 vc -f && vc --prod -f # deploys to vercel
 echo "DEPLOYED TO PRODUCTION ✅"
