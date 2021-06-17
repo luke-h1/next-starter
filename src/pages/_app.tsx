27ffadc984011bcdea48080ee1d96a587ded7c2f
luke-h1 <luke.howsam@yahoo.com>
2021-06-17 21:05:24 +0100

    add gtag hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,22 @@ import Navbar from '@src/components/nav';
 import { Reset } from '@src/styles/Reset';
 import type { AppProps } from 'next/app';
 import '@fontsource/noto-sans-jp';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
+import * as gtag from '@src/utils/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Reset />
