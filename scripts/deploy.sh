2026bb01173bb2a4ae11b3866c4f642697b14d51
luke-h1 <luke.howsam@yahoo.com>
2021-09-29 08:38:02 +0100

    fix build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-npm run build
-npm run format
-npm run lint 
-npm run tsc
-npm run test 
-vc -f && vc --prod -f # deploys to vercel
+vc -f && vc --prod -f
 echo "DEPLOYED TO PRODUCTION ✅"
