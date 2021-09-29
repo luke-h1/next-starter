423186ca3858ec37300bf18dfe1a20aa5d25cc07
luke-h1 <luke.howsam@yahoo.com>
2021-09-29 08:34:58 +0100

    add sitemap and tests

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+};
+declare namespace Cypress {
+  interface Chainable {
+    getByTestId(testId: string): Chainable<Element>;
+    getById(id: string): Chainable<Element>;
+
+  }
+}
---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+const siteUrl = 'https://nextjs-ts-starter-five.vercel.app/';
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+};
